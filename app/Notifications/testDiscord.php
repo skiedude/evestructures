4a25bc49a439ae4bd6fbe98908b804053c9d5d81
Jake Z <skibumb@gmail.com>
2018-03-09 06:57:10 +0000

    new file

---
+++ app/Notifications/testDiscord.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use \DiscordWebhooks\Client;
+use \DiscordWebhooks\Embed;
+use App\Channels\DiscordChannel;
+use Log;
+
+class testDiscord extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(\App\Character $character)
+    {
+      $this->character = $character;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return [DiscordChannel::class];
+    }
+
+
+    public function toDiscord($notifiable) {
+      try {
+        $client = new Client($notifiable->discord_webhook);
+
+        $embed = new Embed();
+        $embed->description(':white_check_mark: **Webhook Test** :white_check_mark:');
+        $embed->title("Test Notification for {$this->character->character_name}");
+        $embed->color( 0x0000a0 );
+        $embed->author(env('APP_NAME'). 'Bot', null, "https://imageserver.eveonline.com/Character/{$notifiable->character_id}_64.jpg");
+        $embed->field('Test Message', 'Ahoy Champion', TRUE);
+
+        $client->username(env('APP_NAME'))
+                ->avatar(env('APP_URL') . "/images/avatar.png")
+                ->embed($embed);
+        Log::debug("Sent Test Discord notification for {$this->character->character_name}");
+        return $client->send();
+      } catch (\Exception $e) {
+        Log::error("Failed to send <TEST> discord notification for {$this->character->character_name} on account $notificable->user_id , $e->getMessage()");
+      }
+    }
+}
